dac7258f83ab504c72362a72b39ad81da099def8
Louis Delleur <louis.delleur@hotmail.com>
2024-09-20 16:08:07 +0200

    style v1.5

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './not-found.component.scss'
 })
 export class NotFoundComponent {
+
+ 
+  @Input() visible:boolean = false;
+  @Input() notFoundMessage: string ='Nothing Found!'
+  @Input() resetLinkText:string = "Reset"
+  @Input() resetLinkRoute:string="/food"
+
 
 }
